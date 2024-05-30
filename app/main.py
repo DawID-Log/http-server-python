b601af59d653415fdfa2c6f7fc10a9a604eb6fdf
DawID-Log <dawid.logoluso@gmail.com>
2024-05-30 15:22:23 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -4,13 +4,21 @@ import socket
 def main():
     crlf = "\r\n"
     statusLine = "HTTP/1.1 200 OK"
-    header = ""
+
+    reqLine = "GET /index.html HTTP/1.1"
+    header_host = "Host: localhost:4221"
+    header_userAgent = "User-Agent: curl/7.64.1"
+    header_accept = "Accept: */*\r\n"
+    header = header_host + crlf + header_userAgent + crlf + header_accept + crlf
     body = ""
 
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b""+statusLine+crlf+crlf)
+    conn, addr = server_socket.accept()  # wait for client
+
+    print(f"connection request from address {addr}")
+    conn.accept()[0].sendall(b""+statusLine+crlf+crlf)
 
 
 if __name__ == "__main__":
