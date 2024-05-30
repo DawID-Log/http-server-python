435c1bc454967a2cae44c9cf39705b5c84fadf6c
DawID-Log <dawid.logoluso@gmail.com>
2024-05-30 14:49:06 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ import socket
 
 
 def main():
-    statusLine = b"HTTP/1.1 200 OK \r\n"
-    header = "\r\n"
+    crlf = "\r\n"
+    statusLine = b"HTTP/1.1 200 OK"
+    header = ""
     body = ""
 
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    server_socket.accept()[0].sendall(statusLine+crlf+crlf)
 
 
 if __name__ == "__main__":
