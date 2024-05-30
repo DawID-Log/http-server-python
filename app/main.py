80fd06a4d791f2f06acd1f98b873ad6330d88cd1
DawID-Log <dawid.logoluso@gmail.com>
2024-05-30 14:45:17 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,11 +2,14 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    statusLine = b"HTTP/1.1 200 OK \r\n"
+    header = "\r\n"
+    body = ""
+
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK \r\n\r\n")
 
 
 if __name__ == "__main__":
