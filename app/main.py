2fa5cee01c6018c9b95c1284763edab5bd39b7af
DawID-Log <dawid.logoluso@gmail.com>
2024-05-31 20:32:00 +0200

    next-stage

--- app/main.py
+++ app/main.py
@@ -105,6 +105,7 @@ def send_request(client):
                         response.encode(),
                         b"\r\n",
                         body,
+                        b"\r\n",
                     ]
                 )
             )
