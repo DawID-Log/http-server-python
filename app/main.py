5380fbce46bd4a914f69a9bc2e28c08c9a510db8
DawID-Log <dawid.logoluso@gmail.com>
2024-05-31 20:15:30 +0200

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -98,7 +98,7 @@ def send_request(client):
 
         body = bodyInEcho if bodyInEcho != "" else userAgent
         if isGzip:
-            body = gzip.compress(body.encode())
+            body = gzip.compress(body)
             print(f"compressed bodye: {body}")
         response += f"\r\n{body}"
         print(f"response: {response}")
